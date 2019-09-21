0be113ebf23d91ea1627a7916a5bdfa873dd4220
Ryan Young <young.ryan1979@gmail.com>
2019-09-21 15:30:41 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  sonic_youth = ["thurston", "kim", "lee", "steve"]
+  sonic_youth.unshift("array")
 end
 
 def remove_element_from_end_of_array(array)
