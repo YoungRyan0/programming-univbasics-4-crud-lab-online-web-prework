1416366e330a4cf3aea9461fb614542fab91d77e
Ryan Young <young.ryan1979@gmail.com>
2019-09-21 15:47:15 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -34,6 +34,8 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
+  array = ["wow", "i", "am", "really", "learning", "arrays!"]
+  array[0]
   
 end
 
